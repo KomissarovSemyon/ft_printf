ff770abd62363c8c8ba37dfa71dbf7139417420d
amerlon- <amerlon-@student.21-school.ru>
2019-01-30 18:37:29 +0300

    [FIX] %p with NULL

--- src/print_pointer.c
+++ src/print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 17:00:02 by amerlon-          #+#    #+#             */
-/*   Updated: 2019/01/30 17:56:42 by amerlon-         ###   ########.fr       */
+/*   Updated: 2019/01/30 18:35:31 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 int	print_pointer(unsigned long long int n, t_token *tok)
 {
-	(void)tok;
 	int		i;
 	int		res;
 	char	*str;
 	char	*hex;
 
 	hex = "0123456789abcdef";
-	str = ft_strnew(14);
+	res = ft_nbrlen_base(n, 16) + 1;
+	str = ft_strnew(res + 2);
 	str[0] = '0';
 	str[1] = 'x';
-	i = 13;
+	i = res;
 	while (i > 1)
 	{
 		str[i] = hex[n % 16];
@@ -32,6 +32,6 @@ int	print_pointer(unsigned long long int n, t_token *tok)
 		i--;
 	}
 	res = print_string(str, tok);
-	free(str);
+	ft_strdel(&str);
 	return (res);
 }
