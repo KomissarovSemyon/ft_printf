cb531b6b8aa018a7ee840683a5a04de40a3e0350
amerlon- <amerlon-@student.21-school.ru>
2019-02-03 13:35:31 +0300

    %jd and %zd support

--- src/print_decimal.c
+++ src/print_decimal.c
@@ -6,7 +6,7 @@
 /*   By: amerlon- <amerlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/30 22:10:07 by amerlon-          #+#    #+#             */
-/*   Updated: 2019/02/02 14:39:31 by amerlon-         ###   ########.fr       */
+/*   Updated: 2019/02/03 13:21:58 by amerlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,10 @@ int	print_decimal(long long int n, t_token *tok)
 		str = ft_ltoa_base((long)n, 10);
 	else if ((tok->flags & F_LL) == F_LL)
 		str = ft_ltoa_base((long long)n, 10);
+	else if ((tok->flags & F_J) == F_J)
+		str = ft_ltoa_base((intmax_t)n, 10);
+	else if ((tok->flags & F_Z) == F_Z)
+		str = ft_ltoa_base((size_t)n, 10);
 	else
 		str = ft_ltoa_base((int)n, 10);
 	// return (ft_putstr(str));
